7fde46ed151d6ec3d7524c026363130026b6d057
Stohes <T.Chies@edu.pucrs.br>
2024-11-02 19:07:43 +0100

    Add more functions to Habit

--- Habit.py
+++ Habit.py
@@ -5,18 +5,43 @@ from datetime import datetime
 @dataclass
 class Habit:
     name: str
-    periodicity: str
+    periodicity_string: str
+    periodicity_int: int
     created_at: datetime
     completion_dates: list
+    current_streak: int
+    max_streak: int
     
     
     def complete_task(self):
-        pass
-    
+        current_date = datetime.now()
+        self.completion_dates.append(current_date)
+        
     
     def is_broken(self):
-        pass
+        last_completion = self.completion_dates[-1]
+        current_date = datetime.now()
+        
+        delta = current_date - last_completion
+        
+        if delta > self.periodicity_int:
+            return True
+        else:
+            return False
+        
+    
+    def get_current_streak(self):
+        return self.current_streak
     
     
-    def streak(self):
-        pass
+    def get_max_streak(self):
+        return self.max_streak
+    
+    
+    def increase_streak(self):
+        self.current_streak += 1
+        
+    
+    def reset_streak(self):
+        self.max_streak = max(self.current_streak, self.max_streak)
+        self.current_streak = 0
