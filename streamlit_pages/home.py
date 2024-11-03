b31b61c3716074a432e40d90bec0bf36de76e704
Stohes <T.Chies@edu.pucrs.br>
2024-11-03 18:17:23 +0100

    Add list of habits to main page

--- streamlit_pages/home.py
+++ streamlit_pages/home.py
@@ -1,3 +1,20 @@
 import streamlit as st
+from HabitManager import HabitManager
 
-st.write("main page 1")
+
+st.title("Habits List")
+
+
+habitManager = HabitManager()
+
+habit_list = habitManager.get_habit_list()
+for habit in habit_list:
+    st.code(
+    f"""
+    Name: {habit.name}
+    Periodicity: {habit.periodicity_string}
+    Current Streak: {habit.current_streak}
+    Max Streak: {habit.max_streak}
+    Completion Dates: {habit.completion_dates}
+    """
+    )
