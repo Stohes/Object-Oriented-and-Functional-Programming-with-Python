efa3aa6f47a04b81056236781b0f8387361cbf82
Stohes <T.Chies@edu.pucrs.br>
2024-11-03 19:14:14 +0100

    Add create habit page

--- streamlit_pages/create_habit.py
+++ streamlit_pages/create_habit.py
@@ -1,3 +1,21 @@
 import streamlit as st
+from HabitManager import HabitManager
 
-st.write("create habit page 2")
+
+st.title("Create Habit")
+
+
+habitManager = HabitManager()
+
+with st.form("Create Habit"):
+    name = st.text_input(label="Name")
+    periodicity = st.selectbox(label="Periodicity", options=["Daily", "Weekly", "Monthly"])
+
+    submitted = st.form_submit_button(label="Create")
+    
+    if submitted:
+        habitManager.create_habit(
+            name=name,
+            periodicity=periodicity
+        )
+        st.write(f"New Habit Successfully Created")
