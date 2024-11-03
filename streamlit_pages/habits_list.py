65d7bc98c1c193f5e961472a434518fa082cb5bc
Stohes <T.Chies@edu.pucrs.br>
2024-11-03 18:21:59 +0100

    Fix indentation

--- streamlit_pages/habits_list.py
+++ streamlit_pages/habits_list.py
@@ -10,11 +10,11 @@ habitManager = HabitManager()
 habit_list = habitManager.habits
 for habit in habit_list:
     st.code(
-    f"""
-    Name: {habit.name}
-    Periodicity: {habit.periodicity_string}
-    Current Streak: {habit.current_streak}
-    Max Streak: {habit.max_streak}
-    Completion Dates: {habit.completion_dates}
-    """
+        f"""
+        Name: {habit.name}
+        Periodicity: {habit.periodicity_string}
+        Current Streak: {habit.current_streak}
+        Max Streak: {habit.max_streak}
+        Completion Dates: {habit.completion_dates}
+        """
     )
