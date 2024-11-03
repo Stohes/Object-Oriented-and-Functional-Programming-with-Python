d46895c84fd1b2e5c7dbbc52dc2123c654c6d258
Stohes <T.Chies@edu.pucrs.br>
2024-11-03 18:18:32 +0100

    Rename home to Habits List

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 pg = st.navigation([
-    st.Page("streamlit_pages/home.py", title="Home"), 
+    st.Page("streamlit_pages/habits_list.py", title="Habits List"), 
     st.Page("streamlit_pages/create_habit.py", title="Create Habit")
 ])
 
