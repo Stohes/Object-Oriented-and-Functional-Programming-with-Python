bbdcca23c8f2c156e18cc659cbe4eaa97b95b3a7
Stohes <T.Chies@edu.pucrs.br>
2024-12-19 23:26:03 +0100

    Fix bug in most_struggled_habits

--- Analytics.py
+++ Analytics.py
@@ -6,13 +6,17 @@ def most_struggled_habits(habits):
     def missed_completions(habit):
         missed = 0
         periodicity_int = habit.get_periodicity_int(habit.periodicity_string)
-        for i in range(1, len(habit.completion_dates)):
-            delta = (habit.completion_dates[i] - habit.completion_dates[i - 1]).days
-            missed += max(0, delta // periodicity_int - 1)
+        sorted_dates = sorted(habit.completion_dates)
+        
+        for i in range(1, len(sorted_dates)):
+            delta = (sorted_dates[i] - sorted_dates[i - 1]).days
+            missed += max(0, (delta // periodicity_int) - 1)
+        
         return missed
     
     struggled_habits = sorted(habits, key=missed_completions, reverse=True)
     return [habit.name for habit in struggled_habits if missed_completions(habit) > 0]
+
 
 
 def current_habits(habits):
